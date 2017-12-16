d79bff18d43e98b71de1d7341df3264813985f07
Sandip Pal <sandip.pal18@gmail.com>
2017-12-16 18:00:49 +0530

    Added QuerySelectorAll

--- vid-2-thinking-globally/js/app.js
+++ vid-2-thinking-globally/js/app.js
@@ -1,7 +1,18 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
 
-myButton.addEventListener('click', () => {
-	myHeading.style.color = myTextInput.value;
-});
+for (let i = 0; i < myList.length; i++) {
+	myList[i].style.color = '#990077';
+}
+
+
+const myListNoOffClass = document.querySelectorAll('.no-off-color');
+
+for (let i = 0; i < myListNoOffClass.length; i++) {
+	myListNoOffClass[i].style.color = '#779900';
+}
+
+const listOdd = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < listOdd.length; i++) {
+	listOdd[i].style.backgroundColor = '#989898';
+}
