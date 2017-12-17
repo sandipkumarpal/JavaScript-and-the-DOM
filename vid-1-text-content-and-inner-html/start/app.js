2d5f65b1a502273be2aa24fa6cfa7c549d036a1a
Sandip Pal <sandip.pal18@gmail.com>
2017-12-17 13:06:42 +0530

    Added Previous Next SelectNode

--- vid-1-text-content-and-inner-html/start/app.js
+++ vid-1-text-content-and-inner-html/start/app.js
@@ -12,6 +12,13 @@ const myRemoveButton = document.querySelector('.myRemoveButton');
 const listItemsLi = document.getElementsByTagName('li');
 const listItemsUl = document.querySelector('ul');
 
+const firstListitem = listItemsUl.firstElementChild;
+const lastListitem = listItemsUl.lastElementChild;
+
+
+firstListitem.style.backgroundColor = 'lightskyblue';
+lastListitem.style.backgroundColor = 'lightsteelblue';
+
 // for (let i = 0; i < listItemsLi.length; i++) {
 // 	listItemsLi[i].addEventListener('mouseover', () => {
 // 		listItemsLi[i].textContent = listItemsLi[i].textContent.toUpperCase();
@@ -21,14 +28,63 @@ const listItemsUl = document.querySelector('ul');
 // 	});
 // }
 
-listItemsUl.addEventListener('mouseover', (event) => {
-	if (event.target.tagName === 'LI') {
-		event.target.textContent = event.target.textContent.toUpperCase();
-	}
-});
-listItemsUl.addEventListener('mouseout', (event) => {
-	if (event.target.tagName === 'LI') {
-		event.target.textContent = event.target.textContent.toLowerCase();
+// listItemsUl.addEventListener('mouseover', (event) => {
+// 	if (event.target.tagName === 'LI') {
+// 		event.target.textContent = event.target.textContent.toUpperCase();
+// 	}
+// });
+// listItemsUl.addEventListener('mouseout', (event) => {
+// 	if (event.target.tagName === 'LI') {
+// 		event.target.textContent = event.target.textContent.toLowerCase();
+// 	}
+// });
+
+
+function attachedListItemButtons(li) {
+	let upButton = document.createElement('button');
+	upButton.className = 'up';
+	upButton.textContent = 'Up';
+	li.appendChild(upButton);
+
+	let downButton = document.createElement('button');
+	downButton.className = 'down';
+	downButton.textContent = 'Down';
+	li.appendChild(downButton);
+
+	let removeButton = document.createElement('button');
+	removeButton.className = 'remove';
+	removeButton.textContent = 'Remove';
+	li.appendChild(removeButton);
+}
+
+for (var i = 0; i < listItemsLi.length; i++) {
+	attachedListItemButtons(listItemsLi[i]);
+}
+
+listItemsUl.addEventListener('click', (event) => {
+	if (event.target.tagName === 'BUTTON') {
+		if (event.target.className === 'remove') {
+			let parent = event.target.parentNode;
+			listItemsUl.removeChild(parent);
+		}
+		if (event.target.className === 'up') {
+			let parent = event.target.parentNode;
+			let previousParent = parent.previousElementSibling;
+			let upperParent = parent.parentNode;
+
+			if (previousParent) {
+				upperParent.insertBefore(parent, previousParent);
+				previousParent.classname = 'highlight';
+			}
+		}
+		if (event.target.className === 'down') {
+			let parent = event.target.parentNode;
+			let nextParent = parent.nextElementSibling;
+			let upperParent = parent.parentNode;
+			if (nextParent) {
+				upperParent.insertBefore(nextParent, parent);
+			}
+		}
 	}
 });
 
@@ -52,6 +108,7 @@ myListButton.addEventListener('click', () => {
 	let li = document.createElement('li');
 	li.textContent = listInput.value;
 	listUl.appendChild(li);
+	attachedListItemButtons(li);
 	listInput.value = '';
 });
 
